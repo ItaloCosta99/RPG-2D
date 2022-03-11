4e4f78cdf74953b4664ed22425a7c3bcd3557f8c
Italo Costa <italocostajobs@gmail.com>
2022-03-11 06:01:56 -0300

    Interface e Atributos

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Entity entity;
+
+    [Header("Player UI")]
+    public Slider health;
+    public Slider mana;
+    public Slider stamina;
+    public Slider exp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        entity.currentHealth = entity.maxHealth;
+        entity.currentMana = entity.maxMana;
+        entity.currentStamina = entity.maxStamina;
+
+        health.maxValue = entity.maxHealth;
+        health.value = health.maxValue;
+
+        mana.maxValue = entity.maxMana;
+        mana.value = mana.maxValue;
+
+        stamina.maxValue = entity.maxStamina;
+        stamina.value = stamina.maxValue;
+
+        exp.value = 0;
+    }
+
+    private void Update()
+    {
+        health.value = entity.currentHealth;
+        mana.value = entity.currentMana;
+        stamina.value = entity.currentStamina;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            entity.currentHealth -= 1;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Player))]
 public class PlayerController : MonoBehaviour
 {
+    public Player player;
     public Animator playerAnimator;
     float input_x = 0;
     float input_y = 0;
-    public float speed = 2.5f;
     bool isWalking = false;
+
+    Rigidbody2D rb2d;
+    Vector2 moviment = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        isWalking = false;   
+        isWalking = false;
+        rb2d = GetComponent<Rigidbody2D>();
+        player = GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -22,18 +29,21 @@ public class PlayerController : MonoBehaviour
         input_x = Input.GetAxisRaw("Horizontal");
         input_y = Input.GetAxisRaw("Vertical");
         isWalking = (input_x != 0 || input_y != 0);
+        moviment = new Vector2(input_x, input_y);
         
         if(isWalking)
         {
-            var move = new Vector3(input_x, input_y, 0).normalized;
-            transform.position += move * speed * Time.deltaTime;
             playerAnimator.SetFloat("input_x", input_x);
             playerAnimator.SetFloat("input_y", input_y);
         }
 
         playerAnimator.SetBool("isWalking", isWalking);
 
-        if (Input.GetButtonDown("Fire1"))
-            playerAnimator.SetTrigger("attack");
+        // if (Input.GetButtonDown("Fire1"))
+        //    playerAnimator.SetTrigger("attack");
+    }
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + moviment * player.entity.speed * Time.fixedDeltaTime); ;
     }
 }
